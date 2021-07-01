0c77a4e4dc45943a88c6938ae3b67b7f5f703f07
kzhu-ME <kevin.zhu@modernelectron.com>
2021-07-01 09:43:21 -0700

    Updated ElectroStaticSolver_ME to include verbosity changes

--- Source/FieldSolver/ElectrostaticSolver_ME.cpp
+++ Source/FieldSolver/ElectrostaticSolver_ME.cpp
@@ -248,10 +248,14 @@ WarpX::computePhiRZ (const amrex::Vector<std::unique_ptr<amrex::MultiFab> >& rho
         rho[lev]->mult(-1._rt/PhysConst::ep0);
     }
 
+    int verbosity = 2;
+    ParmParse pp_warpx("warpx");
+    pp_warpx.query("solver_verbosity", verbosity);
+
     // Solve the Poisson equation
     linop.setDomainBC( lobc, hibc );
     MLMG mlmg(linop);
-    mlmg.setVerbose(2);
+    mlmg.setVerbose(verbosity);
     mlmg.setMaxIter(max_iters);
     mlmg.solve( GetVecOfPtrs(phi), GetVecOfConstPtrs(rho), required_precision, 0.0);
 }
@@ -329,8 +333,12 @@ WarpX::computePhiCartesian (const amrex::Vector<std::unique_ptr<amrex::MultiFab>
         rho[lev]->mult(-1._rt/PhysConst::ep0);
     }
 
+    int verbosity = 2;
+    ParmParse pp_warpx("warpx");
+    pp_warpx.query("solver_verbosity", verbosity);
+
     MLMG mlmg(linop);
-    mlmg.setVerbose(0);
+    mlmg.setVerbose(verbosity);
     mlmg.setMaxIter(max_iters);
     mlmg.solve( GetVecOfPtrs(phi), GetVecOfConstPtrs(rho), required_precision, 0.0);
 }
