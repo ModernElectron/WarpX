f1e7dad4270fd795d82feb0fa2ca7e703a171154
Roelof <roelof.groenewald@modernelectron.com>
2021-07-14 09:24:03 -0700

    pulled assemblies.py over from branch nruof/thermionic_emission

---
+++ mewarpx/mewarpx/assemblies.py
@@ -0,0 +1,56 @@
+"""
+Placeholder for assembly implementations.
+"""
+
+
+class Assembly(object):
+
+    """An assembly represents any shape in the simulation; usually a conductor.
+
+    While V, T, and WF are required, specific use cases may allow None to be
+    used for these fields.
+    """
+
+    def __init__(self, V, T, WF, name):
+        """Basic initialization.
+
+        Arguments:
+            V (float): Voltage (V)
+            T (float): Temperature (K)
+            WF (float): Work function (eV)
+            name (str): Assembly name
+        """
+        self.V = V
+        self.T = T
+        self.WF = WF
+        self.name = name
+
+    def getvoltage(self):
+        """Allows for time-dependent implementations to override this."""
+        return self.V
+
+
+class ZPlane(Assembly):
+
+    """A semi-infinite plane."""
+
+    def __init__(self, z, zsign, V, T, WF, name):
+        """Basic initialization.
+
+        Arguments:
+            z (float): The edge of the semi-infinite plane (m)
+            zsign (int): =1 to extend from z to +inf, or =-1 to extend from
+                -inf to z.
+            V (float): Voltage (V)
+            T (float): Temperature (K)
+            WF (float): Work function (eV)
+            name (str): Assembly name
+        """
+        super(ZPlane, self).__init__(V=V, T=T, WF=WF, name=name)
+
+        self.z = z
+
+        self.zsign = int(round(zsign))
+        if self.zsign not in [-1, 1]:
+            raise ValueError("self.zsign = {} is not either -1 or 1.".format(
+                self.zsign))
