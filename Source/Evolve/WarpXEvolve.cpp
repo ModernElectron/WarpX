1b27a13e1d2fec586e116d35c853c067d3e7cff5
kzhu-ME <kevin.zhu@modernelectron.com>
2021-07-21 16:46:00 -0700

    Removed duplicate warpx_py_particleinjection call

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -166,7 +166,6 @@ WarpX::Evolve (int numsteps)
         if (do_electrostatic != ElectrostaticSolverAlgo::None)
         {
             const bool skip_deposition = true;
-            if (warpx_py_particleinjection) warpx_py_particleinjection();
             PushParticlesandDepose(cur_time, skip_deposition);
         }
         // Electromagnetic case: multi-J algorithm
