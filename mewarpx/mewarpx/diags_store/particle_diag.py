81306807c8a40b3d71e8b97cdf40b954501e591e
nruof <nicholas.ruof@modernelectron.com>
2021-07-22 16:12:25 -0700

    Make Doc Strings for the Particle Diag wrapper

--- mewarpx/mewarpx/diags_store/particle_diag.py
+++ mewarpx/mewarpx/diags_store/particle_diag.py
@@ -1,4 +1,5 @@
 """Diagnostic code that wraps the picmi.ParticleDiagonstics class"""
+
 import logging
 import time
 from mewarpx.diags_store.diag_base import WarpXDiagnostic
@@ -12,11 +13,41 @@ from mewarpx.mwxrun import mwxrun
 logger = logging.getLogger(__name__)
 
 class WarpXParticleDiag(WarpXDiagnostic):
+    """Output particle diagnostics every diagnostic period and produce
+        plots from the diagnostics
 
-    def __init__(self, diag_steps, species=None, data_list=None, **kwargs):
+    Contains:
+        add_particle_diag (function): Function to add the particle diagnostic to
+        the mwxrun.simulation
+
+        post_processing: plot particle diagnostic data, not yet implemented
+    """
+
+    def __init__(self, diag_steps, name=None, species=None,
+                data_list=None, **kwargs):
+
+        """
+        Arguments:
+            Initializes the picmi.ParticleDiagnostic and adds the diagnostic to
+            the simulation
+
+            diag_steps (int): Number of steps between each diagnostic output
+
+            name (str): name of the diag output folder, defaults to diag
+
+            species (mepicmi.Species): species in the simulation, if None then
+            uses all particles in the simulation
+
+            data_list (str list): list of attributes to be outputted by he
+            diagnostic, default uses ["position", "momentum", "weighting"]
+
+        """
+        self.name = name
         self.species = species
         self.data_list = data_list
 
+        if self.name is None:
+            self.name= = "diag"
         if self.species is None:
             self.species = mwxrun.simulation.species
         if self.data_list is None:
@@ -27,7 +58,7 @@ class WarpXParticleDiag(WarpXDiagnostic):
 
     def add_particle_diag(self):
         particle_diag = picmi.ParticleDiagnostic(
-            name="p_diag",
+            name=self.name,
             period=self.diag_steps,
             species=self.species,
             data_list=self.data_list
